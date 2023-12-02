797f06a6cdf0283fc0d780e79d9f875d81b6b9ae
Folafolu <ofolafolu@gmail.com>
2023-12-02 21:11:10 +0100

    initial commit

---
+++ core/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+# Create your models here.
+
+class Category(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    namee = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return f'{self.name}'
+
+class Expense(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    description = models.CharField(max_length=200)
+    amount = models.PositiveIntegerField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f'{self.category} - {self.name}'
