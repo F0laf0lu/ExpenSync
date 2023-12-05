8e449a99ba2f3ab5777938ff678c8473d84ca045
Folafolu <ofolafolu@gmail.com>
2023-12-05 15:19:54 +0100

    filters

--- core/urls.py
+++ core/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('add-expense/', views.addexpense, name='add-expense'),
     path('delete-expense/<int:id>/', views.deleteexpense, name='delete-expense'),
     path('update-expense/<int:id>/', views.updateexpense, name='update-expense'),
-    path('expenses', views.expenses, name='expense'),
+    path('expenses/', views.expenses, name='expense'),
+    path('transactions/search/', views.transationfilters, name='search-filter'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import redirect, render
 from core.models import Expense, Category
 from django.db.models import Sum, Count
+from django.utils import timezone
+import calendar
+from datetime import timedelta   
 
-# Create your views here.
+
+current_date = timezone.now()
+
+def get_month_range(input_date):
+    # Get the first day of the month
+    first_day = input_date.replace(day=1)
+
+    # Calculate the last day of the month
+    _, last_day = calendar.monthrange(input_date.year, input_date.month)
+    last_day = input_date.replace(day=last_day)
+
+    return first_day, last_day
+
+def get_week_range(input_date):
+    # Calculate the start of the week (Monday)
+    start_of_week = input_date - timedelta(days=input_date.weekday())
+
+    # Calculate the end of the week (Sunday)
+    end_of_week = start_of_week + timedelta(days=6)
+
+    return start_of_week, end_of_week
+
+
+# Views
 
 def home(request):
     user_id = request.user.id
@@ -34,12 +60,13 @@ def home(request):
 
 def expenses(request):
     user_id = request.user
-    total = 0
     expenses = Expense.objects.filter(
         user=user_id).order_by("-created_on")
+    
     category = Category.objects.filter(user=user_id)
-    for i in expenses:
-        total += i.amount
+
+    total = Expense.objects.filter(
+            user=user_id).aggregate(Sum('amount'))['amount__sum']
 
     context = {
         'expenses' : expenses,
@@ -48,6 +75,39 @@ def expenses(request):
     }
 
     return render(request, 'expenses.html', context)
+
+def transationfilters(request):
+    user_id = request.user
+    total = Expense.objects.filter(
+            user=user_id).aggregate(Sum('amount'))['amount__sum']
+    expenses = ""
+    # Search Query
+    query = request.GET.get('q')
+
+    if query:
+        expenses = Expense.objects.filter(description__icontains=query)
+        
+
+    # Time Filter
+    timequery = request.GET.get('timefilter')
+    if timequery == 'thisweek':
+        start_week, end_week = get_week_range(current_date)
+        expenses = Expense.objects.filter(created_on__range=(start_week, end_week))
+    if timequery == 'thismonth':
+        start_month, end_month = get_month_range(current_date)
+        expenses = Expense.objects.filter(created_on__range=(start_month, end_month))
+    if timequery == 'thisyear':
+        expenses = Expense.objects.filter(created_on__year=2023) #fix year
+
+    context = {
+    'query':query,
+    'timequery':timequery,
+    'expenses' : expenses,
+    'total':total,
+    'expenses':expenses,
+    }
+    
+    return render(request, "expenses.html", context)
 
 def addexpense(request):
     user = request.user
@@ -74,7 +134,6 @@ def addexpense(request):
 def deleteexpense(request, id):
     expense = Expense.objects.get(id=id)
     category = expense.category
-    print(Expense.objects.filter(category=category).count())
     if request.method == 'POST':
         expense.delete()
         if Expense.objects.filter(category=category).count() == 0:
