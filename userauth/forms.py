e24d229d40931a17083d162e839e244314ccf44d
Folafolu <ofolafolu@gmail.com>
2023-12-11 02:23:58 +0100

    user auth

--- userauth/forms.py
+++ userauth/forms.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-
+from django import forms
 from .models import CustomUser
 
 
@@ -7,11 +7,39 @@ class CustomUserCreationForm(UserCreationForm):
 
     class Meta:
         model = CustomUser
-        fields = ("email",)
+        fields = ["first_name", "last_name", "email"]
+
+    # Customize fields individually
+    first_name = forms.CharField(
+        max_length=30,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'firstname'})
+    )
+
+    last_name = forms.CharField(
+        max_length=30,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'lastname'})
+    )
+
+    email = forms.EmailField(
+        max_length=254,
+        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'email'})
+    )
+
+    password1 = forms.CharField(
+        label="Password",
+        strip=False,
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'password'})
+    )
+
+    password2 = forms.CharField(
+        label="Password confirmation",
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'confirm password'}),
+        strip=False,
+    )
+
 
 
 class CustomUserChangeForm(UserChangeForm):
-
     class Meta:
         model = CustomUser
         fields = ("email",)
--- userauth/views.py
+++ userauth/views.py
@@ -1,3 +1,51 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from . forms import CustomUserCreationForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
+from django.shortcuts import redirect
 
 # Create your views here.
+
+def userlogin(request):
+    User = get_user_model()
+    if request.method == 'POST':
+        email = request.POST.get('email').lower()
+        password = request.POST.get('password')
+        try:
+            user = User.objects.get(email=email)
+        except: 
+            messages.error(request, 'user does not exist')
+
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'email and password do not match')
+    context = {}
+    return render(request, 'userauth/login.html', context)
+
+
+def userlogout(request):
+    logout(request)
+    return redirect('login')
+
+def usersignup(request):
+    signupform = CustomUserCreationForm()
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.email = user.email.lower()
+            user.save()
+            return redirect('login')
+        else:
+            messages.error(request, 'Error during registration')
+
+
+    context = {
+        'signupform':signupform
+    }
+    return render(request, "userauth/signup.html", context)
+
